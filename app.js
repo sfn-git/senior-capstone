6989f3ebdf52b8f7569963c79768921e7a5c8413
Syed Fahad Nadeem <syedfnadeem323@gmail.com>
2020-02-06 20:58:59 -0500

    Node project set-up. Added Readme. Changed routing for html files

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log("Another Test");
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000; //3000 for Development. Can be changed when we are ready to implement. 
+const dir = __dirname;
+
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+
+app.get("/", (req,res)=>{
+
+    res.sendFile(htmlFile('index'));
+
+});
+
+app.get("/student-form", (req,res)=>{
+
+    res.sendFile(htmlFile("student_form"));
+
+})
+
+app.post("/student-form", (req,res)=>{
+
+    console.log("Post requeest from student form works!")
+    res.sendFile(htmlFile("index"));
+
+})
+
+app.listen(port, ()=>console.log(`Server now running at port ${port}`));
+
+
+// Function to make it easier to call a page instead of having to do path.join everytime. Just need the name without the .html
+function htmlFile(page){
+    viewsPath = 'views/' + page + '.html';
+    return path.join(dir, viewsPath);
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+### RADS CAPSTONE GITHUB REPO
+
+## How to test your code/what the hell is going on
+
+1. Clone the repo.  
+`git clone https://github.com/mrfn323/senior-capstone`
+2. Go into the directory and on the command line do...  
+`npm install`  
+
+### The project is now installed onto your machine and is ready for testing. To test the project...
+
+1. Run the node server  
+`node app.js`
+2. Open your preferred web browser and go to localhost:3000
