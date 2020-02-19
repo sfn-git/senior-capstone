9d82a640b9cd9ac159e262c292a799d319fbb36f
reuhdz <30872054+reuhdz@users.noreply.github.com>
2020-02-19 14:59:11 -0500

    Updated signin.js and index.html
    Merged js funcions and removed commented out code on index.html

--- public/js/signin.js
+++ public/js/signin.js
@@ -1,29 +1,3 @@
-/*
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-
-    // The ID token you need to pass to your backend:
-    var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
-
-    document.getElementById('name').innerText = "Signed in: " +
-        profile.getName();
-}
-
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-    });
-}
-*/
-
 var googleUser = {};
 var startApp = function () {
     gapi.load('auth2', function () {
@@ -44,7 +18,28 @@ function attachSignin(element) {
         function (googleUser) {
             document.getElementById('name').innerText = "Signed in: " +
                 googleUser.getBasicProfile().getName();
+
+            var profile = googleUser.getBasicProfile();
+            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+            console.log('Full Name: ' + profile.getName());
+            console.log('Given Name: ' + profile.getGivenName());
+            console.log('Family Name: ' + profile.getFamilyName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+            // The ID token you need to pass to your backend:
+            var id_token = googleUser.getAuthResponse().id_token;
+            console.log("ID Token: " + id_token);
+            
         }, function (error) {
             alert(JSON.stringify(error, undefined, 2));
         });
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+    document.getElementById('name').innerText = "";
 }
