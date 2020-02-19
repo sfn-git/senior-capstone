a402084cc8454940bf82134001a14b8153364890
Syed Fahad Nadeem <syed@syedfahadnadeem.com>
2020-02-19 14:58:37 -0500

    Removed ssh-tunnel stuff. Will create something that works for now and secure it later

--- models/database.js
+++ models/database.js
@@ -1,32 +1,14 @@
 const dbconfig = require('./dbconfig.json');
 const mongoose = require('mongoose');
-const tunnel = require('tunnel-ssh');
 const URL = dbconfig.URL;
 
-var config = {
+// mongoose.connect(URL);
+// var db = mongoose.connection;
 
-    username: dbconfig.user,
-    privateKey: require('fs').readFileSync(dbconfig.key),
-    host: dbconfig.host,
-    port: dbconfig.port,
-    dstPort: dbconfig.port
+// db.on('error', console.log("Error connecting to the Db"));
+// db.once('open', console.log("Connected to the DB"));
 
-}
-
-var server = tunnel(config, (err,ser)=>{
-
-    if(err) console.log(err);
-    console.log("Connected to proxy server");
-
-})
-
-mongoose.connect(URL);
-var db = mongoose.connection;
-
-db.on('error', console.log("Error connecting to the Db"));
-db.once('open', console.log("Connected to the DB"));
-
-db.close();
+// db.close();
 
 // module.exports = {
 
