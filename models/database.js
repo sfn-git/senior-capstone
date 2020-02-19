1b65f652875916aa6c620cb2777591b79655be55
Syed Fahad Nadeem <syed@syedfahadnadeem.com>
2020-02-19 14:10:55 -0500

    Commented parts that are breaking the program

--- models/database.js
+++ models/database.js
@@ -8,11 +8,25 @@ var config = {
     username: dbconfig.user,
     privateKey: require('fs').readFileSync(dbconfig.key),
     host: dbconfig.host,
-    port: dbconfig.port
+    port: dbconfig.port,
+    dstPort: dbconfig.port
 
 }
 
+var server = tunnel(config, (err,ser)=>{
 
+    if(err) console.log(err);
+    console.log("Connected to proxy server");
+
+})
+
+mongoose.connect(URL);
+var db = mongoose.connection;
+
+db.on('error', console.log("Error connecting to the Db"));
+db.once('open', console.log("Connected to the DB"));
+
+db.close();
 
 // module.exports = {
 
