adab3101953d96fdf1ddcd9deece027bee903b27
reuhdz <30872054+reuhdz@users.noreply.github.com>
2020-02-26 13:44:00 -0500

    Updated login to handle non kean emails

--- public/js/signin.js
+++ public/js/signin.js
@@ -16,21 +16,31 @@ function attachSignin(element) {
     console.log(element.id);
     auth2.attachClickHandler(element, {},
         function (googleUser) {
-            document.getElementById('name').innerText = "Signed in: " +
-                googleUser.getBasicProfile().getName();
-
             var profile = googleUser.getBasicProfile();
-            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-            console.log('Full Name: ' + profile.getName());
-            console.log('Given Name: ' + profile.getGivenName());
-            console.log('Family Name: ' + profile.getFamilyName());
-            console.log('Image URL: ' + profile.getImageUrl());
-            console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+            var email = profile.getEmail().split("@");
+            if(email[1] == "kean.edu"){
+                document.getElementById('name').innerText = "Signed in: " +
+                    profile.getName();
 
-            // The ID token you need to pass to your backend:
-            var id_token = googleUser.getAuthResponse().id_token;
-            console.log("ID Token: " + id_token);
-            
+                console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+                console.log('Full Name: ' + profile.getName());
+                console.log('Given Name: ' + profile.getGivenName());
+                console.log('Family Name: ' + profile.getFamilyName());
+                console.log('Image URL: ' + profile.getImageUrl());
+                console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+                // The ID token you need to pass to your backend:
+                var id_token = googleUser.getAuthResponse().id_token;
+                console.log("ID Token: " + id_token);
+
+                document.getElementById("signinBtn").style.display = "none";
+                document.getElementById("signout").classList.add("d-block");
+            }
+            else{
+                alert("Please signin with a kean email");
+                signOut();
+            }
+
         }, function (error) {
             alert(JSON.stringify(error, undefined, 2));
         });
@@ -42,4 +52,6 @@ function signOut() {
         console.log('User signed out.');
     });
     document.getElementById('name').innerText = "";
+    document.getElementById("signinBtn").style.display = "block";
+    document.getElementById("signout").classList.remove("d-block");
 }
