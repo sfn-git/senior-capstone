f03c2eb1a97b6b0f5793709c51eac55b4657277c
Syed Fahad Nadeem <syedfnadeem323@gmail.com>
2020-03-24 13:07:07 -0400

    Changing signin to make it one page instead of being active through out the nav-bar

--- public/js/signin.js
+++ public/js/signin.js
@@ -1,61 +1,63 @@
 var googleUser = {};
-var startApp = function () {
-    gapi.load('auth2', function () {
-        // Retrieve the singleton for the GoogleAuth library and set up the client.
-        auth2 = gapi.auth2.init({
-            client_id: '745501205386-1eib7vvmr41pa488m35bf2f9l88thd72.apps.googleusercontent.com',
-            cookiepolicy: 'single_host_origin',
-            prompt: 'select_account',
-            hosted_domain: 'kean.edu',
-            // Request scopes in addition to 'profile' and 'email'
-            //scope: 'additional_scope'
+    var startApp = function () {
+        gapi.load('auth2', function () {
+            // Retrieve the singleton for the GoogleAuth library and set up the client.
+            auth2 = gapi.auth2.init({
+                client_id: '745501205386-1eib7vvmr41pa488m35bf2f9l88thd72.apps.googleusercontent.com',
+                cookiepolicy: 'single_host_origin',
+                prompt: 'select_account',
+                hosted_domain: 'kean.edu',
+                //scope: 'additional_scope'
+            });
+            attachSignin(document.getElementById('signinBtn'));
         });
-        attachSignin(document.getElementById('signinBtn'));
-    });
-};
+    };
 
-function attachSignin(element) {
-    console.log(element.id);
-    auth2.attachClickHandler(element, {},
-        function (googleUser) {
-            var profile = googleUser.getBasicProfile();
-            document.getElementById('name').innerText = "Signed in: " +
-                profile.getName();
+    function attachSignin(element) {
+        console.log(element.id);
+        auth2.attachClickHandler(element, {},
+            function (googleUser) {
+                var profile = googleUser.getBasicProfile();
 
-            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-            console.log('Full Name: ' + profile.getName());
-            console.log('Given Name: ' + profile.getGivenName());
-            console.log('Family Name: ' + profile.getFamilyName());
-            console.log('Image URL: ' + profile.getImageUrl());
-            console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+                console.log('ID: ' + profile.getId());
+                console.log('Full Name: ' + profile.getName());
+                console.log('Given Name: ' + profile.getGivenName());
+                console.log('Family Name: ' + profile.getFamilyName());
+                console.log('Image URL: ' + profile.getImageUrl());
+                console.log('Email: ' + profile.getEmail());
+                
+                // The ID token you need to pass to your backend:
+                var id_token = googleUser.getAuthResponse().id_token;
 
-            // The ID token you need to pass to your backend:
-            var id_token = googleUser.getAuthResponse().id_token;
-            //console.log("ID Token: " + id_token);
-            //Send ID token to server
-            /*
-            var xhr = new XMLHttpRequest();
-            xhr.open('POST', 'http://localhost:3000');
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            xhr.onload = function(){
-                console.log('Signed in as: ' + xhr.responseText);
-            };
-            xhr.send('idtoken=' + id_token);
-            */
+                //Send ID token to server
+                authenticate(id_token, profile);
 
-            document.getElementById("signinBtn").style.display = "none";
-            document.getElementById("signout").classList.add("d-block");
-        }, function (error) {
-            alert(JSON.stringify(error, undefined, 2));
+            }, function (error) {
+                alert(JSON.stringify(error, undefined, 2));
+            });
+    }
+
+    function signOut() {
+        var auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+            console.log('User signed out.');
+            document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
         });
-}
+        document.getElementById('name').innerText = "";
+        document.getElementById("signinBtn").style.display = "block";
+        document.getElementById("signout").classList.remove("d-block");
+    }
 
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-    });
-    document.getElementById('name').innerText = "";
-    document.getElementById("signinBtn").style.display = "block";
-    document.getElementById("signout").classList.remove("d-block");
-}
+    function authenticate(token, profile) {
+        $.ajax({
+            url: "/signin",
+            type: "POST",
+            data: `idtoken=${token}`,
+            success: (data)=>{
+                console.log(data);
+            },
+            error: ()=>{
+                console.log("An error occured");
+            }
+        })
+    }
