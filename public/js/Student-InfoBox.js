02edbb0f7eff701ae1f9ec63aa5b19269ca445d3
Alexandre Reis <reisal@kean.edu>
2020-03-24 13:06:28 -0400

    fixed similar refrence name issues between navbar and info-Box

--- public/js/Student-InfoBox.js
+++ public/js/Student-InfoBox.js
@@ -3,7 +3,7 @@
 
         for (i = 0; i < coll.length; i++) {
             coll[i].addEventListener("click", function() {
-                this.classList.toggle("active");
+                this.classList.toggle("active1");
                 var content = this.nextElementSibling;
                 if (content.style.maxHeight)
                 {
