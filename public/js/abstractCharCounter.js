c0d95e030ed1b44c67191dd7ab45e2205e18e052
dhenriques <danielmhenriques@outlook.com>
2020-04-03 12:58:16 -0400

    Modified to be slightly more dynamic

--- public/js/abstractCharCounter.js
+++ public/js/abstractCharCounter.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     var text_max = 750;
-    $('#abstractCharRemaining').html(text_max + ' characters remaining of 750.');
+    $('#abstractCharRemaining').html(text_max + ' characters remaining of ' + text_max + '.');
 
     $('#abstract').keyup(function () {
         var text_length = $('#abstract').val().length;
         var text_remaining = text_max - text_length;
 
-        $('#abstractCharRemaining').html(text_remaining + ' characters remaining of 750.');
+        $('#abstractCharRemaining').html(text_remaining + ' characters remaining of ' + text_max + '.');
     });
 });
