39a0450915f989655bc7a654b92f3932a900eefd
Alexandre Reis <reisal@kean.edu>
2020-04-22 16:47:36 -0400

    Hyperlink and hoverable highlight

---
+++ public/js/launchModel.js
@@ -0,0 +1,67 @@
+function launchModel() 
+{
+    document.getElementById("launchModel").innerHTML = `<div id="myModal" class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
+        <div class="modal-content">
+            <div class="container">
+                <div class="modal-header">
+                    <div class="row">
+                        <div class="col-10">
+                            <h3>A Quantitative Analysis of Economic Theory in China</h3>
+                        </div>
+                        <div class="col">
+                            <span class="badge badge-info">Pending ORSP: #3919837 </span>
+                        </div>
+                    </div>
+
+
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-8 mr-auto">
+                            <h5>Abstract</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sodales gravida
+                                augue vitae accumsan. In ultricies enim vel lectus sodales, in venenatis lacus
+                                sollicitudin. In at risus cursus, convallis augue sit amet, commodo dolor. Morbi
+                                venenatis felis neque, non accumsan nisi finibus malesuada. Nunc semper non
+                                massa id
+                                tempus. Etiam blandit lobortis orci in convallis. Pellentesque mattis finibus
+                                nunc
+                                in cursus. Ut in turpis dapibus, fringilla orci sit amet, molestie erat.
+                                Vestibulum
+                                aliquam tempor porta. In finibus risus et sem venenatis, eget dictum felis
+                                aliquam.
+                                Fusce pretium, justo eu elementum porta, lectus dui posuere ante, id tristique
+                                arcu
+                                lectus a augue. Praesent et odio nisl. Aliquam erat volutpat. Sed et magna
+                                nulla.
+                                Sed dictum augue accumsan est egestas, in fringilla diam aliquam. Curabitur
+                                pharetra
+                                sapien neque, sit amet lacinia sem commodo ut. </p>
+                        </div>
+                        <div class="col-3">
+                            <h5>Primary Presenter</h5>
+                            <p>Daniel Henriques</p>
+                            <h5 class="mt-4">Additional Presenters</h5>
+                            <p>Syed Nadeem</p>
+                            <h5 class="mt-4">Faculty</h5>
+                            <p>Ching-yu "Austin" Huang</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <p class="mr-auto small text-muted">
+                        Last edited on 04/15/20
+                    </p>
+                    <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>`;
+  } 
