a49258bd759c2712b4292d4b8a4995be8d2f6a0f
Alexandre Reis <reisal@kean.edu>
2020-04-24 19:48:36 -0400

    Cleaned up all JS for launch modal and added border around text when in edit mode

--- public/js/launchModal.js
+++ public/js/launchModal.js
@@ -2,12 +2,38 @@ function launchModal(title, status, abstract, primary, coPres, faculty, lastEdit
 {
   var status = status;
   var lastEdit = lastEdit;
-    document.getElementById("title").innerHTML = title
-    document.getElementById("status").innerHTML =  "Pending ORSP: #" + status
-    document.getElementById("abstract").innerHTML = abstract
+  document.getElementById("title").innerHTML = title
+  document.getElementById("status").innerHTML =  "Pending ORSP: #" + status
+  document.getElementById("abstract").innerHTML = abstract
 
-    document.getElementById("primary").innerHTML = primary
-    document.getElementById("coPres").innerHTML = coPres
-    document.getElementById("faculty").innerHTML = faculty
-    document.getElementById("lastEdit").innerHTML = "Last edited on " + lastEdit
+  document.getElementById("primary").innerHTML = primary
+  document.getElementById("coPres").innerHTML = coPres
+  document.getElementById("faculty").innerHTML = faculty
+  document.getElementById("lastEdit").innerHTML = "Last edited on " + lastEdit
   } 
+
+function editAbstarct(){
+  document.getElementById("abstract").style.borderColor = "grey";
+}
+
+function doneEditAbstarct(){
+  document.getElementById("abstract").style.borderColor = "white";
+}
+
+function editable(){
+  var h1 = document.getElementsByTagName("p")[0];
+  var att = document.createAttribute("contenteditable");
+  att.value = "true";
+  h1.setAttributeNode(att);
+  }
+  
+function noteditable(){
+  var h1 = document.getElementsByTagName("p")[0];
+  var att = document.createAttribute("contenteditable");
+  att.value = "flase";
+  h1.setAttributeNode(att);
+  }
+  
+  $('edit btn btn-primary').click(function(){
+  $(".my-textbox").focus()
+  });
