a3d263c32a7eaacd106ca9b4d4b2c0976a300fb8
Alexandre Reis <reisal@kean.edu>
2020-04-29 17:12:11 -0400

    Confirms were removed and Upload will only accepts .pptx & .docx

--- public/js/editModal.js
+++ public/js/editModal.js
@@ -5,14 +5,14 @@ $('#edit').click(function() {
 });
 
 $('#save').click(function() {
-    confirm("Confirm changes");
+    // confirm("Confirm changes");
     $(this).siblings('#edit, #approve, #close').show();
     $(this).siblings('#cancel').hide();
     $(this).hide();
 });
 
 $('#cancel').click(function() {
-    confirm("Any changes you made, will not be saved! Do you wish to continue?");
+    // confirm("Any changes you made, will not be saved! Do you wish to continue?");
     $(this).siblings('#edit, #approve, #close').show();
     $(this).siblings('#save').hide();
     $(this).hide();
