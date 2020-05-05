773d27b2e920b05b3a03fe47d41bed75a48af51d
dhenriques <danielmhenriques@outlook.com>
2020-05-05 18:04:00 -0400

    Merge with Fahad: improved modals and badges. Learned that overloading isn't natively supported in JS

--- public/js/launchModal.js
+++ public/js/launchModal.js
@@ -1,12 +1,40 @@
-
 // output information into modal
-function launchModal(step, title, status, abstract, primary, coPres, faculty, lastEdit) 
-{
-  
+
+function launchStudentModal(
+  status,
+  title,
+  id,
+  abstract,
+  primary,
+  coPres,
+  faculty,
+  lastEdit
+) {
   document.getElementById("title").innerHTML = title;
-  document.getElementById("status").innerHTML = step + ": #" + status;
+  document.getElementById("submissionID").innerHTML = status + ": #" + id;
+  document.getElementById("abstractLaunchModal").innerHTML = abstract;
+  document.getElementById("primary").innerHTML = primary;
+  document.getElementById("coPres").innerHTML = coPres;
+  document.getElementById("faculty").innerHTML = faculty;
+  document.getElementById("lastEdit").innerHTML = "Last edited on " + lastEdit;
+}
+
+function launchFacultySubmissionModal(
+  step,
+  title,
+  id,
+  abstract,
+  description,
+  primary,
+  coPres,
+  faculty,
+  lastEdit
+) {
+
+  document.getElementById("title").innerHTML = title;
+  document.getElementById("status").innerHTML = step + ": #" + id;
   // need to know what status their are
-  if(step == "Upload Required"){
+  if (step == "Upload Required") {
     document.getElementById("IPO").style.visibility = "visible";
     $("#customFile").on('change', ()=>{
       console.log(status);
@@ -19,40 +47,39 @@ function launchModal(step, title, status, abstract, primary, coPres, faculty, la
   }
   
   document.getElementById("abstractLaunchModal").innerHTML = abstract;
-
+  document.getElementById("description").innerHTML = description;
   document.getElementById("primary").innerHTML = primary;
   document.getElementById("coPres").innerHTML = coPres;
   document.getElementById("faculty").innerHTML = faculty;
   document.getElementById("lastEdit").innerHTML = "Last edited on " + lastEdit;
-  } 
+}
 
 // Make div border grey to see it when in edit mode
-function editAbstarct(){
+function editAbstarct() {
   document.getElementById("abstractLaunchModal").style.borderColor = "grey";
 }
 
 // Make div border white to hide it when not in edit mode
-function doneEditAbstarct(){
+function doneEditAbstarct() {
   document.getElementById("abstractLaunchModal").style.borderColor = "white";
 }
 
 //make the div editable
-function editable(){
+function editable() {
   var h1 = document.getElementsByTagName("p")[0];
   var att = document.createAttribute("contenteditable");
   att.value = "true";
   h1.setAttributeNode(att);
-  }
-  
-//make the div none editable 
-function noteditable(){
+}
+
+//make the div none editable
+function noteditable() {
   var h1 = document.getElementsByTagName("p")[0];
   var att = document.createAttribute("contenteditable");
   att.value = "flase";
   h1.setAttributeNode(att);
-  }
-  
-  $('edit btn btn-primary').click(function(){
-  $(".my-textbox").focus()
-  });
+}
 
+$("edit btn btn-primary").click(function () {
+  $(".my-textbox").focus();
+});
