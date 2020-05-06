6415c581b1024e04791b8108c87c97920c4d6b40
Syed Fahad Nadeem <syedfnadeem323@gmail.com>
2020-05-06 17:17:55 -0400

    Cleaning up
    Added some data validation to models
    Cleaned up some unneeded console.logs.

--- models/students.js
+++ models/students.js
@@ -3,9 +3,9 @@ const schema = mongoose.Schema;
 
 var studentsSchema = new schema({
 
-    name: {type: String},
-    stuID: {type: Number, unique: true},
-    email: {type: String, unique: true},
+    name: {type: String, require = true},
+    stuID: {type: Number},
+    email: {type: String, unique: true, require = true},
     major: {type: mongoose.Types.ObjectId},
     classLevel: {type: String},
     primaryLocation: {type: String}
