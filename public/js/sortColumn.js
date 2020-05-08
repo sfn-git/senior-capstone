e375ad1c8a0817d4d7a36531d4d3c9c95f35ab39
Alexandre Reis <reisal@kean.edu>
2020-05-08 15:20:11 -0400

    Sort columns when clicking on category for ORSP

---
+++ public/js/sortColumn.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#sortColumn').dataTable({
+        "bPaginate": false,
+        "bLengthChange": false,
+        "bFilter": true,
+        "bInfo": false,
+        "bAutoWidth": false });
+    });
