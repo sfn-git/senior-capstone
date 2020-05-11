940b838f9e675268acc2fdf3b04e6fc4ce0b36f2
Alexandre Reis <reisal@kean.edu>
2020-05-10 21:56:03 -0400

    Fixed ORSP admin tool that got removed during a merge; notes,approve,deny and css all functioning on Admin dashboard.

--- public/js/editModalAdminORSP.js
+++ public/js/editModalAdminORSP.js
@@ -1,53 +1,59 @@
 // orsp
-$('#adminNote').click(function() {
+$('#adminNote').click(function () {
     $(this).hide();
     $(this).siblings('#adminClose, #adminApprove, #adminDeny').hide();
     $(this).siblings('#adminSave, #adminCloseNote').show();
+    document.getElementById("AdminNoteLaunchModal").style.borderColor = "grey";
+    document.getElementById("AdminNoteLaunchModal").readOnly = false;
 });
 
-$('#adminSave').click(function() {
-    if($("#AdminNoteLaunchModal").val() == noteGlobal){
+$('#adminSave').click(function () {
+    if ($("#AdminNoteLaunchModal").val() == noteGlobal) {
         $(this).siblings('#adminNote, #adminApprove, #adminDeny, #adminClose').show();
         $(this).siblings('#adminCloseNote').hide();
         $(this).hide();
-    }else{
+        document.getElementById("AdminNoteLaunchModal").style.borderColor = "white";
+        document.getElementById("AdminNoteLaunchModal").readOnly = true;
+    } else {
         $.ajax({
 
             method: "POST",
             url: "/orsp-add-note-faculty",
-            data: {id: idGlobal, note: $("#AdminNoteLaunchModal").val()},
-            success: (res)=>{
-                if(res.status){
+            data: { id: idGlobal, note: $("#AdminNoteLaunchModal").val() },
+            success: (res) => {
+                if (res.status) {
                     location.reload();
-                }else{
+                } else {
                     window.alert(`Something went wrong adding your note: ${res.message}`)
                 }
             },
-            error: ()=>{
+            error: () => {
                 window.alert("Could not save note");
             }
         })
     }
 });
 
-$('#adminCloseNote').click(function() {
+$('#adminCloseNote').click(function () {
     // confirm("Any changes you made, will not be saved! Do you wish to continue?");
     $(this).siblings('#adminNote, #adminApprove, #adminDeny, #adminClose').show();
     $(this).siblings('#adminSave').hide();
     $(this).hide();
+    document.getElementById("AdminNoteLaunchModal").style.borderColor = "white";
+    document.getElementById("AdminNoteLaunchModal").readOnly = true;
 });
 
-$('#adminApprove').click(()=> {
-    if(window.confirm(`Approve Project ID: ${idGlobal}?`)){
+$('#adminApprove').click(() => {
+    if (window.confirm(`Approve Project ID: ${idGlobal}?`)) {
         $.ajax({
             method: "POST",
             url: "/orsp-approve-faculty",
-            data: {id: idGlobal},
+            data: { id: idGlobal },
             success: (res) => {
-                if(res.status){
+                if (res.status) {
                     window.alert(`${idGlobal} has been marked as approved.`)
                     location.reload();
-                }else{
+                } else {
                     window.alert(res.message);
                 }
             }
@@ -55,23 +61,23 @@ $('#adminApprove').click(()=> {
     }
 });
 
-$('#adminDeny').click(()=>{
+$('#adminDeny').click(() => {
 
-    if(window.confirm(`Are you sure you would like to redact project #${idGlobal}?`)){
+    if (window.confirm(`Are you sure you would like to redact project #${idGlobal}?`)) {
 
         $.ajax({
             method: "POST",
             url: "/orsp-deny-faculty",
-            data: {id: idGlobal},
-            success:(res) =>{
-                if(res.status){
+            data: { id: idGlobal },
+            success: (res) => {
+                if (res.status) {
                     window.alert(`${idGlobal} has been denied.`);
                     window.location.reload();
-                }else{
+                } else {
                     window.alert(res.message);
                 }
             },
-            error:(err)=>{
+            error: (err) => {
                 alert("Something went wrong.");
             }
         })
